3cf15ac58d20c73806c6777787873e470dde1342
Iksan Maricar <iksan.maricar@futuready.com>
2019-03-28 16:41:19 +0700

    change deploy script

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,5 +1,5 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/fbl/be && npm install
-node index.js & 1&2 > /dev/null
+node index.js &> /dev/null
 
 # cd /home/ec2-user/fbl/fe && npm install && npm run serve
