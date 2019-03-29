ff9dd8c5853c2c49d88c451d479b964d23463144
Iksan Maricar <iksan.maricar@futuready.com>
2019-03-29 09:33:02 +0700

    add insertandfetch

--- service/product/insert.js
+++ service/product/insert.js
@@ -2,9 +2,9 @@ const product = require('../../model/product');
 
 module.exports = async (post) => {
     
-    const product_list = await product.query().upsertGraph(post);
+    const product_list = await product.query().upsertGraphAndFetch(post);
     if(product_list){
-        return {success: true};
+        return {success: true, product_list};
     } else {
         return {success: false};
     }
